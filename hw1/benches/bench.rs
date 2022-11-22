974756059ac26ebf30327cfa4f0269b2e4acdc5b
thor <thorck@protonmail.com>
2022-11-21 22:35:42 -0800

    vignere

--- hw1/benches/bench.rs
+++ hw1/benches/bench.rs
@@ -1,26 +1,24 @@
 //! Benches. To use, import functions of interest, and `cargo bench`.
 //!
-//! `iai` is an experimental benchmark harness, using Cachegrind to ferform precise single-shot
-//! measurements. https://bheisler.github.io/criterion.rs/book/iai/iai.html
+//! https://bheisler.github.io/criterion.rs/book/iai/iai.html
 #![allow(unused_imports)]
 use std::{thread, time};
 
 use criterion::{black_box as bb, criterion_group, criterion_main, Criterion};
-use uncloak_hw::{rsa_decrypt, rsa_encrypt, setup, Setup};
+use uncloak_hw::RSA;
 
 // 260 microseconds on my machine
 fn bench_en(c: &mut Criterion) {
-  let setup = setup();
-  c.bench_function("encrypt", |f| {
-    f.iter(|| rsa_encrypt(setup.pub_key.clone(), b"some_text", rand::thread_rng()))
-  });
+  let rsa = RSA::new();
+
+  c.bench_function("encrypt", |f| f.iter(|| rsa.encrypt(b"some_text", rand::thread_rng())));
 }
 
 // 2.2 millisecond on my machine
 fn bench_de(c: &mut Criterion) {
-  let setup = setup();
-  let ciphertext: Vec<u8> = rsa_encrypt(setup.pub_key, b"some text", rand::thread_rng());
-  c.bench_function("decrypt", |f| f.iter(|| rsa_decrypt(setup.priv_key.clone(), &ciphertext)));
+  let rsa = RSA::new();
+  let ciphertext: Vec<u8> = rsa.encrypt(b"some text", rand::thread_rng());
+  c.bench_function("decrypt", |f| f.iter(|| rsa.decrypt(&ciphertext)));
 }
 
 criterion_group!(benches, bench_en, bench_de);
--- hw1/src/lib.rs
+++ hw1/src/lib.rs
@@ -9,26 +9,101 @@ use rsa::{PaddingScheme, PublicKey, RsaPrivateKey, RsaPublicKey};
 
 const BITS: usize = 2048;
 
-pub struct Setup {
+#[derive(Debug, Clone)]
+pub struct RSA {
   pub priv_key: RsaPrivateKey,
   pub pub_key:  RsaPublicKey,
 }
 
-pub fn setup() -> Setup {
-  let priv_key = RsaPrivateKey::new(&mut rand::thread_rng(), BITS).expect("failed to generate a key");
-  let pub_key = RsaPublicKey::from(&priv_key);
-  Setup { priv_key, pub_key }
+impl RSA {
+  pub fn new() -> Self {
+    let priv_key =
+      RsaPrivateKey::new(&mut rand::thread_rng(), BITS).expect("failed to generate a key");
+    let pub_key = RsaPublicKey::from(&priv_key);
+    Self { priv_key, pub_key }
+  }
+
+  pub fn encrypt<R: CryptoRng + RngCore>(&self, data: &[u8], mut rng: R) -> Vec<u8> {
+    self
+      .pub_key
+      .encrypt(&mut rng, PaddingScheme::new_pkcs1v15_encrypt(), data)
+      .expect("failed to encrypt")
+    // assert_ne!(&data[..], &enc_data[..]);
+  }
+
+  pub fn decrypt(&self, enc_data: &[u8]) -> Vec<u8> {
+    self
+      .priv_key
+      .decrypt(PaddingScheme::new_pkcs1v15_encrypt(), enc_data)
+      .expect("failed to decrypt")
+  }
 }
 
-pub fn rsa_encrypt<R: CryptoRng + RngCore>(
-  pub_key: RsaPublicKey,
-  data: &[u8],
-  mut rng: R,
-) -> Vec<u8> {
-  pub_key.encrypt(&mut rng, PaddingScheme::new_pkcs1v15_encrypt(), data).expect("failed to encrypt")
-  // assert_ne!(&data[..], &enc_data[..]);
-}
+pub mod vignere {
 
-pub fn rsa_decrypt(priv_key: RsaPrivateKey, enc_data: &[u8]) -> Vec<u8> {
-  priv_key.decrypt(PaddingScheme::new_pkcs1v15_encrypt(), enc_data).expect("failed to decrypt")
+  #[derive(Clone, Debug)]
+  pub struct Vignere<'a> {
+    key: &'a [u8],
+  }
+
+  fn check_alphabetic(s: &str) -> anyhow::Result<()> {
+    for c in s.chars() {
+      match c {
+        'a'..='z' => (),
+        _ => return Err(anyhow::anyhow!("Invalid character in key, must be lowercase a-z: {c}")),
+      }
+    }
+    Ok(())
+  }
+
+  impl<'a> Vignere<'a> {
+    pub fn new(key: &'a str) -> anyhow::Result<Self> {
+      check_alphabetic(key)?;
+      Ok(Self { key: key.as_bytes() })
+    }
+
+    pub fn encrypt(&self, plaintext: &str) -> anyhow::Result<Vec<u8>> {
+      check_alphabetic(plaintext)?;
+      let key_it = self.key.iter().cycle();
+      let out = std::iter::zip(plaintext.bytes(), key_it)
+        .map(|(p, k)| {
+          let p = p - b'a';
+          let k = k - b'a';
+          let c = (p + k) % 26;
+          c + b'a'
+        })
+        .collect::<Vec<_>>();
+      Ok(out)
+    }
+
+    pub fn decrypt(&self, ciphertext: &str) -> anyhow::Result<String> {
+      check_alphabetic(ciphertext)?;
+      let key_it = self.key.iter().cycle();
+      Ok(
+        std::iter::zip(ciphertext.bytes(), key_it)
+          .map(|(p, k)| {
+            let p = p - b'a';
+            let k = k - b'a';
+            let c = (26 + p - k) % 26;
+            c + b'a'
+          })
+          .map(|c| c as char)
+          .collect(),
+      )
+    }
+  }
+
+  #[cfg(test)]
+  mod test {
+    use super::*;
+    #[test]
+    fn test_vig() {
+      let msg = "aoeuidhtnsqjkxbmwvzpyfgcrl";
+      let key = "averygoodkey";
+      let v = Vignere::new(key).unwrap();
+      let ciphertext = v.encrypt(msg).unwrap();
+      let plaintext = v.decrypt(&String::from_utf8(ciphertext).unwrap()).unwrap();
+      assert_eq!(msg, plaintext);
+    }
+  }
 }
